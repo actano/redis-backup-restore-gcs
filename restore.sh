51995c8f8e82ef7b719d5a7ccba3ce8d8c8e04bc
Wasiq Kashkari <w.kashkari@outlook.com>
2019-09-27 18:08:10 +0200

    PA-1681: update the backup script so it runs outside of docker

--- restore.sh
+++ restore.sh
@@ -11,6 +11,16 @@ if [ -z $BACKUP_FULL_NAME ]; then
     exit 1
 fi
 
+if [ -z $POD_NAME ]; then
+    echo "You must specify a redis pod name to uplod the backup to"
+    exit 1
+fi
+
+if [ -z $NAMESPACE ]; then
+    echo "You must specify a namespace"
+    exit 1
+fi
+
 echo "Activating google credentials before beginning"
 gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
 
@@ -24,18 +34,19 @@ echo "Beginning restore from $GCS_BUCKET_REDIS/$BACKUP_FULL_NAME.rdb.tar.gz to t
 echo "============================================================"
 
 echo "Fetch dump from google"
-gsutil cp "$GCS_BUCKET_REDIS/$BACKUP_FULL_NAME.rdb.tar.gz" "/restore/dump.tar.gz"
+mkdir -p ./restore
+gsutil cp "$GCS_BUCKET_REDIS/$BACKUP_FULL_NAME.rdb.tar.gz" "./restore/dump.tar.gz"
+
 echo "Unpacking tar file"
-cd /restore
+cd ./restore
 tar -xzf ./dump.tar.gz
+
+echo "Renaming ./backup/${BACKUP_FULL_NAME} to dump.rdb"
 cp ./backup/${BACKUP_FULL_NAME}.rdb ./dump.rdb
 
-echo "Remove the old dump file if it exists"
-mkdir -p /volume/data
-rm -rf /volume/data/dump.rdb
-
-echo "Copy new dump to volume"
-cp /restore/dump.rdb /volume/data/dump.rdb
-
-echo "Redis backup restored"
+echo "================= Copying Backup to Pod ===================="
+echo "Coping /restore/dump.tar.gz to $POD_NAME:/data in $NAMESPACE"
+echo "============================================================"
+kubectl -n $NAMESPACE cp ./dump.tar.gz $POD_NAME:/data
+cd ../
 exit $?
