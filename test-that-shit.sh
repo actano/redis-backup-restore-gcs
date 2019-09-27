c7b2b35c0daa9bae57a1f6789365020d76f8feb7
Wasiq Kashkari <w.kashkari@outlook.com>
2019-09-27 18:17:40 +0200

    PA-1681: update test script

--- test-that-shit.sh
+++ test-that-shit.sh
@@ -1,1 +1,5 @@
-docker run -v "$(pwd)/creds:/creds" -v "$(pwd)/tmp:/restore"  -v "$(pwd)/volume:/volume" -e GCS_BUCKET_REDIS=gs://allex-events-redis-staging-backups -e BACKUP_FULL_NAME=redis_backup_rplan_staging-2019-09-27T010012Z -e GOOGLE_APPLICATION_CREDENTIALS=/creds/creds.json rest
+GCS_BUCKET_REDIS=gs://allex-events-redis-staging-backups \
+BACKUP_FULL_NAME=redis_backup_rplan_staging-2019-09-27T010012Z \
+NAMESPACE=staging \
+POD_NAME=events-redis-staging-redis-ha-server-0 \
+./restore.sh
