2043d322962d906f330430ed940340e59e2d9ef2
Wasiq Kashkari <w.kashkari@outlook.com>
2019-09-27 11:37:36 +0200

    PA-1681: get restore.sh to work

--- restore.sh
+++ restore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e pipefail
 
 if [ -z $GCS_BUCKET_REDIS ]; then
     echo "You must specify a google cloud storage GCS_BUCKET_REDIS address such as gs://my-backups/"
@@ -19,14 +20,21 @@ if [ $? -ne 0 ] ; then
 fi
 
 echo "=============== Redis Backup RESTORE ======================="
-echo "Beginning restore from $GCS_BUCKET_REDIS/$BACKUP_FULL_NAME to the volume mapped to /restore"
+echo "Beginning restore from $GCS_BUCKET_REDIS/$BACKUP_FULL_NAME.rdb.tar.gz to the volume mapped to /restore"
 echo "============================================================"
 
 echo "Fetch dump from google"
-gsutil cp "$GCS_BUCKET_REDIS/$BACKUP_FULL_NAME" "/restore/dump.tar.gz"
-tar -xfz /restore/dump.tar.gz /restore/dump.rdb
+gsutil cp "$GCS_BUCKET_REDIS/$BACKUP_FULL_NAME.rdb.tar.gz" "/restore/dump.tar.gz"
+echo "Unpacking tar file"
+cd /restore
+tar -xzf ./dump.tar.gz
+cp ./backup/${BACKUP_FULL_NAME}.rdb ./dump.rdb
 
-echo "Copy dump to volume"
+echo "Remove the old dump file if it exists"
+mkdir -p /volume/data
+rm -rf /volume/data/dump.rdb
+
+echo "Copy new dump to volume"
 cp /restore/dump.rdb /volume/data/dump.rdb
 
 echo "Redis backup restored"
--- test-that-shit.sh
+++ test-that-shit.sh
@@ -1,2 +1,1 @@
-docker run -v "$(pwd)/creds:/creds" -v "$(pwd)/tmp:/restore"  -e GCS_BUCKET_REDIS=gs://rplan-redis-backups -e BACKUP_FULL_NAME=redis_backup_rplan_staging-2019-08-12T160023Z.rdb.tar.gz -e GOOGLE_APPLICATION_CREDENTIALS=/creds/creds.json rest
-
+docker run -v "$(pwd)/creds:/creds" -v "$(pwd)/tmp:/restore"  -v "$(pwd)/volume:/volume" -e GCS_BUCKET_REDIS=gs://allex-events-redis-staging-backups -e BACKUP_FULL_NAME=redis_backup_rplan_staging-2019-09-27T010012Z -e GOOGLE_APPLICATION_CREDENTIALS=/creds/creds.json rest
