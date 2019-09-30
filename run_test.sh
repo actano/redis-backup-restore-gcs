d4a4ae21fd0218b34ec80877ee1247f5071deccc
Daniel Merz <Daniel.Merz@Actano.de>
2019-09-30 14:59:26 +0200

    PA-1681: Remove jenkinsfile & run_test
    So the repo can be made public

--- run_test.sh
+++
@@ -1,7 +0,0 @@
-# /bin/bash
-
-GCS_BUCKET_REDIS=gs://allex-events-redis-staging-backups \
-BACKUP_FULL_NAME=redis_backup_rplan_staging-2019-09-27T010012Z \
-NAMESPACE=staging \
-POD_NAME=events-redis-staging-redis-ha-server-0 \
-./restore.sh
